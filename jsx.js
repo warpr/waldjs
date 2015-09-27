acb3075f19e39e8af0a2906a6c8ee996c72bb94c
kuno <kuno@frob.nl>
2015-09-27 07:44:06 -0500

    Reference wald-view correctly

---
+++ jsx.js
@@ -0,0 +1,11 @@
+/**
+ *   This file is part of wald:meta.
+ *   Copyright (C) 2015  Kuno Woudt <kuno@frob.nl>
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of copyleft-next 0.3.0.  See LICENSE.txt.
+ */
+
+'use strict';
+
+exports.view = require('wald-view/jsx');
